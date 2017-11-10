791b72b2cf3a35ae212d7dc2fd9c8fc9f8f52fcc
Tomasz Mackiewicz <Tomasz.Mackiewicz@altkom.pl>
2017-11-10 16:10:09 +0100

    News carousel

--- web/js/news-carousel.js
+++ web/js/news-carousel.js
@@ -18,11 +18,15 @@ $(document).ready(function() {
         }
         clickEvent = false;
     });
-});
-
-$(window).load(function() {
     var boxheight = $('#myCarousel .carousel-inner').innerHeight();
     var itemlength = $('#myCarousel .item').length;
     var triggerheight = Math.round(boxheight/itemlength+1);
-    $('#myCarousel .list-group-item').outerHeight(triggerheight);
+    $('#myCarousel .list-group-item').outerHeight(triggerheight);    
 });
+
+//$(window).load(function() {
+//    var boxheight = $('#myCarousel .carousel-inner').innerHeight();
+//    var itemlength = $('#myCarousel .item').length;
+//    var triggerheight = Math.round(boxheight/itemlength+1);
+//    $('#myCarousel .list-group-item').outerHeight(triggerheight);
+//});
