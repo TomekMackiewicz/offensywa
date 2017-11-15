cdad2acbc38e905c1ea4a20cd1cc942139c2a5ef
Tomasz Mackiewicz <Tomasz.Mackiewicz@altkom.pl>
2017-11-15 11:24:05 +0100

    Register sonata services

---
+++ src/AppBundle/Admin/GameAdmin.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class GameAdmin extends Admin
+{
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+                ->add('home_team', 'text')
+                ->add('away_team', 'text');
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper->add('location');
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+                ->addIdentifier('id')
+                ->addIdentifier('location');
+//                ->addIdentifier('home_team')
+//                ->addIdentifier('away_team');
+    }
+}
