eb441b2789742d57e6835660d9978ed33a7ffc5a
Tomasz Mackiewicz <tomasz.mackiewicz79@gmail.com>
2017-11-25 00:10:53 +0100

    Training crud

---
+++ src/AppBundle/Controller/TrainingController.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Training;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Training controller.
+ */
+class TrainingController extends Controller
+{
+    /**
+     * Lists all training entities.
+     *
+     * @Route("/trainings", name="training_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $trainings = $em->getRepository('AppBundle:Training')->findAll();
+
+        return $this->render('training/index.html.twig', array(
+            'trainings' => $trainings,
+        ));
+    }
+
+    /**
+     * Lists all training entities.
+     *
+     * @Route("/admin/trainings", name="admin_training_index")
+     * @Method("GET")
+     */
+    public function adminIndexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $trainings = $em->getRepository('AppBundle:Training')->findAll();
+        $deleteForms = array();
+        foreach($trainings as $training) {
+            $deleteForms[$training->getId()] = $this->createDeleteForm($training)->createView();
+        }
+        
+        return $this->render('training/admin-index.html.twig', array(
+            'trainings' => $trainings,
+            'deleteForms' => $deleteForms,
+        ));
+    }     
+    
+    /**
+     * Creates a new training entity.
+     *
+     * @Route("/admin/trainings/new", name="training_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $training = new Training();
+        $form = $this->createForm('AppBundle\Form\TrainingType', $training);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($training);
+            $em->flush();
+
+            return $this->redirectToRoute('training_show', array('id' => $training->getId()));
+        }
+
+        return $this->render('training/new.html.twig', array(
+            'training' => $training,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a training entity.
+     *
+     * @Route("/trainings/{id}", name="training_show")
+     * @Method("GET")
+     */
+    public function showAction(Training $training)
+    {
+        $deleteForm = $this->createDeleteForm($training);
+
+        return $this->render('training/show.html.twig', array(
+            'training' => $training,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing training entity.
+     *
+     * @Route("/admin/{id}/edit", name="training_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Training $training)
+    {
+        $deleteForm = $this->createDeleteForm($training);
+        $editForm = $this->createForm('AppBundle\Form\TrainingType', $training);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('training_edit', array('id' => $training->getId()));
+        }
+
+        return $this->render('training/edit.html.twig', array(
+            'training' => $training,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a training entity.
+     *
+     * @Route("/{id}", name="training_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Training $training)
+    {
+        $form = $this->createDeleteForm($training);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($training);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('training_index');
+    }
+
+    /**
+     * Creates a form to delete a training entity.
+     *
+     * @param Training $training The training entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Training $training)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('training_delete', array('id' => $training->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
---
+++ src/AppBundle/Form/TrainingType.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+class TrainingType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('date', DateTimeType::class, [
+                'widget' => 'single_text',
+                'format' => 'dd-MM-yyyy HH:mm',
+                'attr' => [
+                    'class' => 'form-control input-inline datetimepicker',
+                    'data-provide' => 'datetimepicker'
+                ],
+                'label' => 'Data'
+            ])
+            ->add('location', null, array(
+                'label' => 'Miejsce'
+            ))
+            ->add('team', EntityType::class, array(
+                'class' => 'AppBundle:Team',
+                'choice_label' => 'name',
+                'multiple' => false,
+                'expanded' => false,
+                'placeholder' => 'Wybierz...',
+                'label' => 'Drużyna'
+            ))
+            ->add('trainer', EntityType::class, array(
+                'class' => 'AppBundle:Trainer',
+                'choice_label' => 'lastName',
+                'multiple' => false,
+                'expanded' => false,
+                'placeholder' => 'Wybierz...',
+                'label' => 'Trener'
+            ));
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Training'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_training';
+    }
+
+
+}
