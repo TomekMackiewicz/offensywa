0f52e876b4e57771cfb8ccb487284baf603a7038
Tomasz Mackiewicz <tomasz.mackiewicz79@gmail.com>
2017-12-05 19:38:43 +0100

    Move calendar trainings to another function

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -12,6 +12,39 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
  */
 class AdminController extends Controller
 {
+    
+    private function getCalendarTrainings($trainings)
+    {
+        $calendarTrainings = [];
+        $currentDate = date('Y-m-d');        
+        $firstDayOfMonth = date("Y-m-01", strtotime($currentDate));
+        $lastDayOfMonth = date("Y-m-t", strtotime($currentDate));
+        
+        foreach ($trainings as $training) {
+            $counter = new \DateTime($firstDayOfMonth);            
+            $end = new \DateTime($lastDayOfMonth); 
+            
+            while ($counter <= $end) {
+                if($counter->format("N") == $training->getDay()) {
+                    $startx = new \DateTime($counter->format("Y-m-d H:i"));
+                    $startx->setTime($training->getStartHour()->format('H'), 0);
+                    $endx = new \DateTime($counter->format("Y-m-d H:i"));
+                    $endx->setTime($training->getEndHour()->format('H'), 0);
+                    
+                    $calendarTrainings[] = [
+                        'title' => 'Trening',
+                        'start' => $startx->format("Y-m-d H:i"),
+                        'end' => $endx->format("Y-m-d H:i"),
+                        'allDay' => false
+                    ];                   
+                }              
+                $counter->modify('+1 day');                
+            }            
+        }
+        
+        return $calendarTrainings;
+    }
+    
     /**
      * Dashboard.
      *
@@ -25,44 +58,14 @@ class AdminController extends Controller
         $playersCount = $em->getRepository('AppBundle:Player')->countPlayers();
         $teamsCount = $em->getRepository('AppBundle:Team')->countMyTeams();
         $thisMonthPayments = $em->getRepository('AppBundle:Payment')->getThisMonthPayments();
-        
         $tasks = $em->getRepository('AppBundle:Task')->findAll(); // current month? year?
-        $trainings = $em->getRepository('AppBundle:Training')->findAll(); // current month? year? wtedy to poniżej niepotrzebne  
-        
-        $currentDate = date('Y-m-d');
-        $jsonTrainings = [];
-        $firstDayOfMonth = date("Y-m-01", strtotime($currentDate));
-        $lastDayOfMonth = date("Y-m-t", strtotime($currentDate));
-        
-        foreach ($trainings as $training) {
-
-            $counter = new \DateTime($firstDayOfMonth);            
-            $end = new \DateTime($lastDayOfMonth); 
-            
-            while ($counter <= $end) {
-                if($counter->format("N") == $training->getDay()) {
-                    $startx = new \DateTime($counter->format("Y-m-d H:i"));
-                    $startx->setTime($training->getStartHour()->format('H'), 0);
-                    $endx = new \DateTime($counter->format("Y-m-d H:i"));
-                    $endx->setTime($training->getEndHour()->format('H'), 0);
-                    
-                    $jsonTrainings[] = [
-                        'title' => 'Trening',
-                        'start' => $startx->format("Y-m-d H:i"),
-                        'end' => $endx->format("Y-m-d H:i"),
-                        'allDay' => false
-                    ];                   
-                }              
-                $counter->modify('+1 day');                
-            }            
-        }
+        $trainings = $em->getRepository('AppBundle:Training')->findAll(); // current month? year? wtedy to poniżej niepotrzebne 
+        $calendarTrainings = $this->getCalendarTrainings($trainings);
 
         return $this->render('admin/dashboard.html.twig', array(
             'tasks' => $tasks,
-            'jsonTrainings' => $jsonTrainings,
+            'calendarTrainings' => $calendarTrainings,
             'trainings' => $trainings,
-            'first' => $firstDayOfMonth,
-            'last' => $lastDayOfMonth,
             'playersCount' => $playersCount,
             'teamsCount' => $teamsCount,
             'thisMonthPayments' => $thisMonthPayments
